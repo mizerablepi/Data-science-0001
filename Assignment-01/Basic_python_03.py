6bf621fbb1b2165195f5eabc968b923e3f4bddfc
miz <mizerablepi@gmail.com>
2022-10-06 23:53:36 +0530

    add Q3

---
+++ Assignment-01/Basic_python_03.py
@@ -0,0 +1,10 @@
+'''
+Q3. Write a Python program to display the first and last colours from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+'''
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print(f'First color : {color_list[0]}')
+
+print(f'Last color : {color_list[-1]}')
