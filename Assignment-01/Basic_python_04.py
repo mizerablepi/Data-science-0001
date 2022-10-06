bdfb33aca8823e61864b5cbcf7ffdee84e5915fd
miz <mizerablepi@gmail.com>
2022-10-07 00:01:48 +0530

    add Q4

---
+++ Assignment-01/Basic_python_04.py
@@ -0,0 +1,10 @@
+'''
+Q4. Write a Python program to print the documents (syntax, description etc.) of Python built-in
+function(s).
+Sample function : abs()
+Expected Result : mat
+abs(number) -> number
+Return the absolute value of the argument.
+'''
+
+print(input.__doc__)
