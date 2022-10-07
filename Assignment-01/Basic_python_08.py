0c6c61a2b7492feb96025e49d4029c75aba58bd9
miz <mizerablepi@gmail.com>
2022-10-07 19:14:33 +0530

    add Q8

--- Assignment-01/Basic_python_08.py
+++ Assignment-01/Basic_python_08.py
@@ -1,3 +1,8 @@
 '''
 Q8. Write a Python program to create a histogram from a given list of integers.
 '''
+
+array = [int(x.strip()) for x in input().split(',')]
+
+for i in array:
+    print(f'{i}','*' * i)
