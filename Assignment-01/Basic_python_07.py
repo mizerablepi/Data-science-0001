8528d4d5c9483efcfdf8cebfeba63cb7e157e811
miz <mizerablepi@gmail.com>
2022-10-07 18:24:08 +0530

    add Q7

--- Assignment-01/Basic_python_07.py
+++ Assignment-01/Basic_python_07.py
@@ -4,3 +4,10 @@ Test Data :
 3 -> [1, 5, 8, 3] : True
 -1 -> [1, 5, 8, 3] : False
 '''
+
+array = [1, 5, 8, 3]
+print("Enter a number to check: ")
+
+input = int(input())
+
+print(True if input in array else False)
