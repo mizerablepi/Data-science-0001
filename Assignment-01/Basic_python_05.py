2a40c1b738a5b361bab4ad5bbb7ca5710e8125da
miz <mizerablepi@gmail.com>
2022-10-07 16:10:51 +0530

    add Q5

--- Assignment-01/Basic_python_05.py
+++ Assignment-01/Basic_python_05.py
@@ -2,3 +2,12 @@
 Q5. Write a Python program to print the calendar of a given month and year.
 Note : Use 'calendar' module.
 '''
+
+# importing the calendar module
+import calendar
+
+print("Enter Year")
+year = int(input())
+print("Enter month")
+month = int(input())
+print(calendar.month(year, month))
